94f21392f93fecb8fa5bd153cf76e33dd739aec3
shashi <shashi.kamle99@gmail.com>
2024-01-09 19:26:49 +0530

    added tf files

--- terra.sh
+++ terra.sh
@@ -6,4 +6,4 @@ terraform --version
 terraform init
 terraform validate
 #terraform plan --var-file=dev.tfvars
-terraform apply --var-file=dev.tfvars --auto-approve
+terraform destroy --var-file=dev.tfvars --auto-approve
