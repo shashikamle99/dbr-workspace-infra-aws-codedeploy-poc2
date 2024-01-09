5fbd664ea612aa5f8335fed3c1b58568122afe0a
shashi <shashi.kamle99@gmail.com>
2024-01-09 11:23:33 +0530

    added tf files

--- scripts/terra-workspace.sh
+++ scripts/terra-workspace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 terraform init
 terraform validate
-terraform plan --ver-file=dev.tfvars
-terraform apply --ver-file=dev.tfvars --auto-approve
+terraform plan --var-file=dev.tfvars
+terraform apply --var-file=dev.tfvars --auto-approve
