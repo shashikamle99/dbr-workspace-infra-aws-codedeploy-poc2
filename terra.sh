f292fa918a56d9d6e2f890483832d19556449d13
shashikamle99 <154310776+shashikamle99@users.noreply.github.com>
2024-01-10 19:28:41 +0530

    Update terra.sh

--- terra.sh
+++ terra.sh
@@ -8,6 +8,6 @@ terraform --version
 terraform init
 terraform validate
 #terraform plan --var-file=dev.tfvars
-terraform apply --var-file=dev.tfvars --auto-approve
-#terraform destroy --var-file=dev.tfvars --auto-approve
+#terraform apply --var-file=dev.tfvars --auto-approve
+terraform destroy --var-file=dev.tfvars --auto-approve
 
