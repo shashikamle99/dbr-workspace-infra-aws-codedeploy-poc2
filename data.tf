00943f3b929f2fb3c254071031c012c16b49f5c5
shashi <shashi.kamle99@gmail.com>
2024-01-10 21:47:33 +0530

    added tf files

--- data.tf
+++ data.tf
@@ -1,14 +1,16 @@
-data "aws_ssm_parameter" "client_id" {
-  name = "dbr_client_id"
-}
+# data "aws_ssm_parameter" "client_id" {
+#   name = "dbr_client_id"
+# }
 
-data "aws_ssm_parameter" "client_secret" {
-  name = "dbr_client_secret"
-}
+# data "aws_ssm_parameter" "client_secret" {
+#   name = "dbr_client_secret"
+# }
 
-data "aws_ssm_parameter" "dbr_account_id" {
-  name = "databricks_account_id"
-}
+# data "aws_ssm_parameter" "dbr_account_id" {
+#   name = "databricks_account_id"
+# }
 
+
+data "aws_ssm_parameter" "aws_ssm" {}
 
 
--- output.tf
+++ output.tf
@@ -14,7 +14,7 @@ output "databricks_account_id" {
 }
 
 locals {
-    client_id = data.aws_ssm_parameter.client_id.value
-    client_secret = data.aws_ssm_parameter.client_secret.value
-    databricks_account_id = data.aws_ssm_parameter.dbr_account_id.value
+    client_id = data.aws_ssm_parameter.dbr_client_id.value
+    client_secret = data.aws_ssm_parameter.dbr_client_secret.value
+    databricks_account_id = data.aws_ssm_parameter.databricks_account_id.value
 }
