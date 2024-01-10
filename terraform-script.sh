4e0c622195088a770afd87ca62483ab039ea0c13
shashi <shashi.kamle99@gmail.com>
2024-01-10 22:09:49 +0530

    added tf files

--- terraform-script.sh
+++ terraform-script.sh
@@ -3,6 +3,7 @@ cd ~
 cd /opt/codedeploy-agent/infra-code
 terraform --version
 terraform init
+terraform fmt
 terraform validate
 #terraform plan --var-file=dev.tfvars
 #terraform apply --var-file=dev.tfvars --auto-approve
