d84f46086b7613f00137766e5f4808e08c09d0e3
shashikamle99 <154310776+shashikamle99@users.noreply.github.com>
2024-01-11 21:08:44 +0530

    Update workspace-script.sh

--- workspace-script.sh
+++ workspace-script.sh
@@ -6,6 +6,6 @@ terraform init
 terraform fmt
 terraform validate
 #terraform plan --var-file=dev.tfvars
-terraform apply --var-file=dev.tfvars --auto-approve
-#terraform destroy --var-file=dev.tfvars --auto-approve
+#terraform apply --var-file=dev.tfvars --auto-approve
+terraform destroy --var-file=dev.tfvars --auto-approve
 
