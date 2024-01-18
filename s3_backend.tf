0df7c5beb4b7019408287158d51c7dbf2ce22580
shashi <shashi.kamle99@gmail.com>
2024-01-18 20:08:37 +0530

    update changes

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terra-backend-s3"
-    key    = "db-workspace"
-    region = "us-east-2"
+    bucket = "dbr-s3-bucket"
+    key    = "terra-backend-poc-1"
+    region = "us-east-1"
   }
 }
